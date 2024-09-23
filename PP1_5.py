ec6842f4714f1fc29038fa2c2b40c42e418ada9f
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-09-23 13:52:17 +0000

    test

--- PP1_5.py
+++ PP1_5.py
@@ -1,19 +1,25 @@
 
 def q1():
   #Write Assignment code here
+  Num= input("Input a integer: ")
+  Num= int(Num)
+  print(f"Your integer + 3 is: {Num + 3} ")
+ 
 
 def q2():
   #Write Assignment code here
+  num1= input("Input a number: ")
+  num1
 
 def q3():
   #Write Assignment code here
-
+  print(1)
 def q4():
   #Write Assignment code here
-
+  print(1)
 def q5():
   #Write Assignment code here
-
+  print(1)
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
