65662cfa84558807420aae1a16af62a73a14212a
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-10-06 22:02:57 +0000

    done

--- PP1_5.py
+++ PP1_5.py
@@ -1,24 +1,28 @@
 
 def q1():
   #Write Assignment code here
-  Num= input("Input a integer: ")
-  Num= int(Num)
-  print(f"Your integer + 3 is: {Num + 3} ")
- 
+  user = input("Input an integer: ")
+  number = int(user)
+  result = number + 3  
+  print(result)
 
 def q2():
   #Write Assignment code here
-  num1= input("Input a number: ")
-  num1= float(num1) + 4
-  print(f"Your number + 2 is: {num1 + 2} ")
+  num1 = input("Input a number: ")
+  num2 = num1 + '4'
+  float_number = float(num2)
+  result = float_number + 2
+  print(result)
+
 
 def q3():
   #Write Assignment code here
-  num2= input("Input a radius: ")
-  radius = float(num2)
-  Pi= 3.14
-  area= Pi*(radius**2)
-  print(f"The area of a circle is: ",area)
+  radius1 = input("Input a radius: ")
+  radius = float(radius1)
+  pi = 3.14
+  area = pi * (radius ** 2)
+  print(area)
+
 def q4():
   #Write Assignment code here
   word= input("Input a number: ")
@@ -26,19 +30,21 @@ def q4():
   twelve= 12
   word2= twelve*word
   word2= int(word2)
-  print(f"Your whole number * 12 is: ",word2)
+  print(word2)
 
 def q5():
   #Write Assignment code here
-  num3= input("Input a integer: ")
-  num3= int(num3) + 5
-  print(f"Your number + 5 is {num3}"  )
+  user_input = input("Input an integer: ")
+  number = int(user_input)
+  result = number + 5
+  print(f"Your number + 5 is {result}")
+
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
